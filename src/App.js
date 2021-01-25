47cb877970ecfa47d96bb8062895533c0f5b02c2
Andriy Kikot <andray838@gmail.com>
2021-01-25 22:10:19 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { getContacts } from './redux/contacts/contacts-selectors';
+import Container from './components/Container';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
+import NumberContacts from './components/NumberContacts';
+
 import './App.css';
 
 function App() {
+  const contacts = useSelector(getContacts);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="main__title">Phonebook</h1>
+        <ContactForm />
+
+        {contacts.length > 0 ? (
+          <div>
+            <h2 className="section__title">Contacts</h2>
+            <Filter />
+            <ContactList />
+            <NumberContacts />
+          </div>
+        ) : (
+          <p>Your phone book is empty :(</p>
+        )}
+
+        <ToastContainer autoClose={3000} />
+      </Container>
     </div>
   );
 }
